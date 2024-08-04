503472c3bc746b33ff45dde5b90c582dfe11704e
Mohamed Aslam I <121491315+IMdAslaM@users.noreply.github.com>
2024-08-04 13:07:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from langchain_helper import create_vector_db, get_qa_chain
 
 
 st.markdown("""# Welcome to <span style="color:red">redBus</span> chatbot🤖""", unsafe_allow_html=True)
+st.markdown(" ")
 
+file_ = open("redBus_Gif.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+st.markdown(
+f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+unsafe_allow_html=True,
+)
+
+st.markdown(" ")
+st.markdown(" ")
+st.markdown(" ")
 question = st.text_input("Enter your Query on redBus: ")
 
 if question:
